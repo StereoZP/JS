23730f3a99d46cae152f2eda50f7f43211205c92
Alexander <example@mail.com>
2021-10-13 18:19:21 +0300

    add trainee projects

---
+++ Task.js
@@ -0,0 +1,135 @@
+/*
+let num=3
+alert(num)
+
+let a=10
+let b=2
+num = a+b
+num = a-b
+num = a*b
+num = a/b
+alert(num)
+
+let c=15
+let d=2
+result=c+d
+alert(result)
+
+let a=10,b=2,c=5
+sum= a+b+c
+alert(sum)
+
+let a=17
+let b=10
+c= a-b
+let d=7
+result =c+d
+alert(result)
+
+let str="Hello, World"
+alert(str)
+
+let str1="Hello,"
+let str2="World"
+alert(str1+str2)
+
+let name="Sasha"				//not working
+alert("Hello, %name%")
+
+let age=22						//not working
+alert("Your age %age%")
+
+let name = prompt('Your name?')
+alert('Your name: '+name)
+
+let num = prompt('Write num') 
+alert('Write num: '+num**2)
+
+let str = 'abcde'
+alert(str[0])
+alert(str[1])
+alert(str[4])
+
+let num='12345'
+alert(num[0]*num[1]*num[2]*num[3]*num[4])
+
+alert(60*60)
+alert(60*60*24)
+alert(60*60*24*30)
+
+let hour = prompt('Write hour') 
+let min = prompt('Write min')
+let sec = prompt('Write sec')
+alert(hour+':'+min+':'+sec)
+
+let num=2
+alert(num**2)
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+var num = 10;
+num = num++;
+num = ++num;
+num = num--;
+alert(num);
+
+let a=0
+if (a==0){
+	alert('Right')
+}
+else{
+	alert('Wrong')
+}
+
+let a=0
+if (a>=0){
+	alert('Right')
+}
+else{
+	alert('Wrong')
+}
+
+let a=0
+if (a<=0){
+	alert('Right')
+}
+else{
+	alert('Wrong')
+}
+
+let a=0
+if (a!==0){
+	alert('Wrong')
+}
+else{
+	alert('Right')
+}
+
+let a = 1; 
+if (a === 1) alert('Right!'); else alert('Wrong!');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
