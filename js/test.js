999db7a2877b93648c2c86e21c02ae8b91f7e248
Vladimir Ozirskiy <vladimir.ozirskiy@neogara.com>
2021-10-20 18:53:03 +0300

    Improve solution

--- js/test.js
+++ js/test.js
@@ -1,44 +1,21 @@
-const a=0
-const b=8
-const c=4
+const a = 0
+const b = 1
+const c = 4
 
-function quadratic(a,b,c){
+function solveQuadraticEquation(a, b, c) {
+  if (a === 0) {
+    return NaN;
+  }
+  const D = b ** 2 - 4 * a * c;
+  const x1 = ((-b) ** 2 + D) / (2 * a);
+  const x2 = ((-b) ** 2 - D) / (2 * a);
+  return [x1, x2];
+}
 
-    if (a===0){
-        return 'a cannot equal 0';
-    }
-    else{
-        const discr=b**2-4*a*c
-        const x1 = (b**2+discr)/(2*a)
-        const x2 =(b**2-discr)/(2*a)
-        return [x1,x2];   
-    }
- }
-  console.log(`X1 and X2: ${quadratic(a,b,c)}`);
+const roots = solveQuadraticEquation(a, b, c);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (roots) {
+  console.log(`X1 and X2: ${roots}`);
+} else {
+  console.log('There are no roots');
+}
