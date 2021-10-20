dbcc0b5bb2e8f2a4530da87c72eae244be1610ba
Vladimir Ozirskiy <vladimir.ozirskiy@neogara.com>
2021-10-20 19:18:17 +0300

    Add sale func

--- js/test.js
+++ js/test.js
@@ -1,7 +1,3 @@
-const a = 0
-const b = 1
-const c = 4
-
 function solveQuadraticEquation(a, b, c) {
   if (a === 0) {
     return NaN;
@@ -12,10 +8,28 @@ function solveQuadraticEquation(a, b, c) {
   return [x1, x2];
 }
 
-const roots = solveQuadraticEquation(a, b, c);
+/*
+3) Вычислить сумму покупки с учетом скидки.
+Скидка 3% предоставляется, если сумма покупки больше 500 грн.,
+а скидка 5% - если сумма покупки больше 800 грн.
+Сумму покупки вводит юзер.
+*/
 
-if (roots) {
-  console.log(`X1 and X2: ${roots}`);
-} else {
-  console.log('There are no roots');
+function getSale(price) {
+  if (price > 500 && price <= 800) {
+    return 3;
+  } else if (price > 800) {
+    return 5;
+  }
+  return 0;
 }
+
+function getSalePrice(price) {
+  const sale = getSale(price);
+  return price - ((price / 100) * sale);
+}
+
+console.log(getSalePrice(300), '300');
+console.log(getSalePrice(550), '533.5');
+console.log(getSalePrice(800), '776');
+console.log(getSalePrice(1000), '950');
